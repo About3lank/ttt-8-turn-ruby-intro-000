00b6de60fbc64ff6114647ccd9eeff0fed7260ea
Matthew Blank <msb277@cornell.edu>
2020-05-10 14:48:32 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  until valid_move?(board, index)
+    puts "Please enter 1-9:"
+  end
+  move(board, index)
+  display_board(board)
+end
+
+def display_board(board)
+  puts " #{ board[0] } | #{ board[1] } | #{ board[2] } "
+  puts "-----------"
+  puts " #{ board[3] } | #{ board[4] } | #{ board[5] } "
+  puts "-----------"
+  puts " #{ board[6] } | #{ board[7] } | #{ board[8] } "
+end
+
+def valid_move?(board, index)
+  if 0 <= index && 8 >= index
+    position_open(board, index)
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_open?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, x_or_o = "X")
+  board = board
+  board[index] = x_or_o
+  board
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
