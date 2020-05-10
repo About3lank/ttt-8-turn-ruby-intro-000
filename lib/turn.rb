bc4b1092e6bf449783021de4e20bd27c942f0aea
Matthew Blank <msb277@cornell.edu>
2020-05-10 14:51:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,7 @@
 def display_board(board)
-
+  puts " #{ board[0] } | #{ board[1] } | #{ board[2] } "
+  puts "-----------"
+  expect(output).to include(" O | X | X ")
+  expect(output).to include("-----------")
+  expect(output).to include(" O | X | O ")
 end
